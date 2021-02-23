ed9ab383344d882fb261ea7101e582a2bf54f45a
Radina <56393689+radinan@users.noreply.github.com>
2021-02-24 01:26:44 +0200

    update Calendar.cpp
    -fixed calendar's form : 3 months in a row
    -added operator*(), createCalendar ()and printCalendar()

--- Calendar.cpp
+++ Calendar.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
 #include <cassert>
+#include <iomanip>
+#include <sstream>
+#include <queue>
 
 //using Zeller's algorithm
 int findDay(int dayOfMonth, int monthNumber, int year)
 {
+	//validation
 	assert(monthNumber > 0 && monthNumber < 13);
-	assert(year > 0);
+	assert(year >= 1582); //Gregorian calendar was introduced in October 1582
 	switch (monthNumber)
 	{
 	case 2:		
@@ -24,29 +28,25 @@ int findDay(int dayOfMonth, int monthNumber, int year)
 	default: assert(false);
 	}
 
-
-	int monthZellerNumber = monthNumber, yearZeller = year; //March = 1, April = 2 ... January = 11, February = 12; 
+	int monthZellerNumber = 0, yearZeller = year; //March = 1, April = 2 ... January = 11, February = 12; 
 	if (monthNumber > 2 && monthNumber < 13)
 		monthZellerNumber = monthNumber - 2;
 	else
 	{
 		monthZellerNumber = monthNumber + 10;
-		yearZeller--;
+		--yearZeller;
 	}
 
-	int lastTwoDigitsOfYear = yearZeller % 100,
-		firstTwoDigitsOfYear = yearZeller;
+	int lastTwoDigitsOfYear = yearZeller % 100, firstTwoDigitsOfYear = yearZeller;
 	while (firstTwoDigitsOfYear > 99)
 		firstTwoDigitsOfYear /= 10;
 	
-	int funcZeller = dayOfMonth + ((13 * monthZellerNumber - 1) / 5) + lastTwoDigitsOfYear + (lastTwoDigitsOfYear / 4)
-		- 2 * firstTwoDigitsOfYear + (firstTwoDigitsOfYear / 4);
-
+	int funcZeller = dayOfMonth + (13 * monthZellerNumber - 1) / 5 + lastTwoDigitsOfYear + (lastTwoDigitsOfYear / 4)
+															   - 2 * firstTwoDigitsOfYear + (firstTwoDigitsOfYear / 4);
 	int remainder = funcZeller % 7;
 	while (remainder < 0)
-	{
 		remainder += 7;
-	}
+	
 
 	switch (remainder)
 	{
@@ -76,12 +76,8 @@ std::string monthName(int monthNumber)
 	case 10: return "October";
 	case 11: return "November";
 	case 12: return "December";
-	//default: assert(false);
+	default: assert(false);
 	}
-}
-void printDaysNames()
-{
-	std::cout << "Sun" << " " << "Mon" << " " << "Tue" << " " << "Wed" << " " << "Thu" << " " << "Fri" << " " << "Sat" << std::endl;
 }
 int daysOfMonth(int monthNumber, int year)
 {
@@ -99,43 +95,119 @@ int daysOfMonth(int monthNumber, int year)
 	default: assert(false);
 	}
 }
-void printCalendar(int year)
+std::string operator*(const std::string& lhs, const std::string& rhs)
 {
-	for (int i = 1; i <= 12; ++i) //month
-	{
-		std::cout << monthName(i) << std::endl;
-		printDaysNames();
+	std::stringstream s1(lhs), s2(rhs);
+	std::string merged, lhs2, rhs2, spaces;
+	for (int i = 0; i < (28 + 3); ++i)//28 spaces + 3 for separation
+		spaces += " ";
 
+	if (lhs.size() > rhs.size())
+	{
+		while (std::getline(s1, lhs2))
+		{
+			if (std::getline(s2, rhs2))
+				merged += lhs2 + "   " + rhs2 + "\n"; //3 spaces for separation
+			else
+				merged += lhs2 + spaces + "\n"; 
+		}
+	}
+	else
+	{
+		while (std::getline(s2, rhs2))
+		{
+			if(std::getline(s1, lhs2))
+				merged += lhs2 + "   " + rhs2 + "\n"; //3 spaces for separation
+			else
+				merged += spaces + rhs2 + "\n";
+		}
+	}
+
+	return merged;
+}
+std::vector<std::string> createCalendar(int year) //28 symbs width -> 14th, 15th - in the middle
+{
+	std::vector<std::string> allMonths;
+
+	for (int i = 1; i <= 12; ++i)
+	{
+		std::stringstream s;
+
+		//Name of the month - centrally aligned
+		std::string nameOfMonth = monthName(i);
+		int spacesBeforeName = 0, spacesAfterName = 0, nameLength = nameOfMonth.size(); 
+		if (nameLength % 2 == 0)
+		{
+			spacesBeforeName = 14 - (nameLength - 2) / 2; 
+			spacesAfterName = spacesBeforeName; 
+		}
+		else
+		{
+			spacesBeforeName = 14 - (nameLength - 1) / 2; 
+			spacesAfterName = spacesBeforeName + 1;
+		}
+		for (int j = 1; j < spacesBeforeName; ++j) //1-9 = 9
+			s << " ";
+		s << nameOfMonth; 
+		for (int j = 1; j < spacesAfterName; ++j) //10
+			s << " ";
+		s << "\n";
+
+		//Names of the weekdays
+		s <<"Sun" << " " << "Mon" << " " << "Tue" << " " << "Wed" << " " << "Thu" << " " << "Fri" << " " << "Sat" << " " << "\n";
+
+		//Days of the month
 		int date = 1, day = findDay(1, i, year), lastDay = daysOfMonth(i, year), counter = 0;
-		
 		for (int k = 1; k < day; ++k)
 		{
-			std::cout << "   ";
-			std::cout << " ";
+			s << "   "; //3 spaces
+			s << " ";
 			++counter;
 		}
 		for (int n = 1; n <= lastDay; ++n)
 		{
 			if (counter == 7)
 			{
-				std::cout << std::endl;
+				s << "\n"; 
 				counter = 0;
 			}
 			if (n > 0 && n < 10)
-				std::cout << " " << n << " ";
+				s << " " << n << " "; //3 chars
 			else
-				std::cout << n << " ";
-			std::cout << " ";
+				s << n << " ";
+			s << " ";
 			counter++;
 		}
+		for (int k = counter; k < 7 ; ++k)
+		{
+			s << "   "; 
+			s << " ";
+			++counter;
+		}
+		s << "\n";
 
-		std::cout << std::endl << std::endl;
+		allMonths.push_back(s.str());
+	}
 
+	return allMonths;
+}
+void printCalendar(int year, std::ostream& out)
+{
+	//89 width
+	std::vector<std::string> vec(createCalendar(year));
+	out << std::setw(47) << "Year" << "\n";
+	out << std::setw(47) << year << "\n" << "\n";
+
+	for (int i = 0; i < 12; i += 3)
+	{
+		out << vec[i] * vec[i + 1] * vec[i + 2];
+		out << std::endl;
 	}
 }
+
 int main()
 {
-	printCalendar(2020);
+	printCalendar(1970, std::cout);
 
 	return 0;
 }
